4ea74ac5f4cfd3b8322dc59b6ea1cf9c5d10e02a
college-demo <sharjeelahmad666777@gmail.com>
2025-10-17 14:54:53 -1200

    Fix: added https:// to backend API endpoints

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,7 +1,7 @@
-export const USER_API_END_POINT = 'job-backend-xi-roan.vercel.app/api/v1/user';
-export const JOB_API_END_POINT = 'job-backend-xi-roan.vercel.app/api/v1/job';
-export const APPLICATION_API_END_POINT = 'job-backend-xi-roan.vercel.app/api/v1/application';
-export const COMPANY_API_END_POINT = 'job-backend-xi-roan.vercel.app/api/v1/company';
+export const USER_API_END_POINT = 'https://job-backend-xi-roan.vercel.app/api/v1/user';
+export const JOB_API_END_POINT = 'https://job-backend-xi-roan.vercel.app/api/v1/job';
+export const APPLICATION_API_END_POINT = 'https://job-backend-xi-roan.vercel.app/api/v1/application';
+export const COMPANY_API_END_POINT = 'https://job-backend-xi-roan.vercel.app/api/v1/company';
 
 
 // 'http://localhost:8000/api/v1/user'
